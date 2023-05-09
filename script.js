00f3fa3528804c5d72bdfef1303f2bd3c010f3f5
Claudio Fritzen <92340418+ClaudioFritzen@users.noreply.github.com>
2023-05-09 19:32:08 +0100

    Finalizemos a parte inicial com as animações

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["FullStack Developer", "Youtuber", "Python e Django" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+
+})
